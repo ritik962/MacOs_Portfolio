313de91745605ac26c1543fdc851fcf93b9acc83
RITIK ROSHAN <140687856+ritik962@users.noreply.github.com>
2025-11-22 17:59:18 +0530

    dock: implement dock section

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,9 +1,63 @@
+import { Tooltip } from "react-tooltip";
 import { dockApps } from "#constants";
 import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Dock = () => {
   const toggleApp = (app) => {};
   const dockRef = useRef(null);
+
+  useGSAP(() => {
+    const dock = dockRef.current;
+
+    if (!dock) return;
+
+    const icons = dock.querySelectorAll(".dock-icon");
+
+    const animateIcons = (mouseX) => {
+      const { left } = dock.getBoundingClientRect();
+
+      icons.forEach((icon) => {
+        const { left: iconLeft, width } = icon.getBoundingClientRect();
+        const center = iconLeft - left + width / 2;
+        const distance = Math.abs(mouseX - center);
+        const intensity = Math.exp(-(distance ** 2.5) / 20000);
+
+        gsap.to(icon, {
+          scale: 1 + 0.25 * intensity,
+          y: -15 * intensity,
+          duration: 0.2,
+          ease: "power1.out",
+        });
+      });
+    };
+
+    const handleMouseMove = (e) => {
+      const { left } = dock.getBoundingClientRect();
+
+      animateIcons(e.clientX - left);
+    };
+
+    const resetIcons = () => {
+      icons.forEach((icon) =>
+        gsap.to(icon, {
+          scale: 1,
+          y: 0,
+          duration: 0.3,
+          ease: "power1.out",
+        })
+      );
+    };
+
+    dock.addEventListener("mousemove", handleMouseMove);
+    dock.addEventListener("mouseleave", resetIcons);
+
+    return () => {
+      dock.removeEventListener("mousemove", handleMouseMove);
+      dock.removeEventListener("mouseleave", resetIcons);
+    };
+  }, []);
   return (
     <section id="dock">
       <div ref={dockRef} className="dock-container">
@@ -28,6 +82,8 @@ const Dock = () => {
             </button>
           </div>
         ))}
+
+        <Tooltip id="dock-tooltip" place="top" className="tooltip" />
       </div>
     </section>
   );
